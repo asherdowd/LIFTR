31592de66d37269450676ee43773f09294f81af9
asherdowd <sethdowd@gmail.com>
2026-01-14 23:22:27 -0500

    Add program data models to model container in LIFTRApp.

--- LIFTR/LIFTRApp.swift
+++ LIFTR/LIFTRApp.swift
@@ -8,22 +8,19 @@ struct LiftrApp: App {
             ContentView()
         }
         .modelContainer(for: [
-            // User & Settings
             User.self,
             GlobalProgressionSettings.self,
             ExerciseProgressionSettings.self,
-            
-            // Equipment Inventory
             PlateItem.self,
             BarItem.self,
             CollarItem.self,
-            
-            // Strength Training (Legacy Progression System)
+            Program.self,
+            TrainingDay.self,
+            ProgramExercise.self,
+            ExerciseSession.self,
             Progression.self,
             WorkoutSession.self,
-            WorkoutSet.self,  // Shared: used by both Progression and future Program systems
-            
-            // Cardio Training
+            WorkoutSet.self,
             CardioProgression.self,
             CardioSession.self
         ])
